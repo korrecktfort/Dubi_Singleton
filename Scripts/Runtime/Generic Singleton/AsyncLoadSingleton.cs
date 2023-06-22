c07b2e58b6219cb5c1af0467abdb032fff7ed2a8
Klemens <30729612+korrecktfort@users.noreply.github.com>
2023-06-22 09:15:00 +0200

    Adding Files

---
+++ Scripts/Runtime/Generic Singleton/AsyncLoadSingleton.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using System;
+
+namespace Dubi.Singleton
+{
+    public abstract class AsyncLoadSingleton<T, U> : Singleton<T> where U : Component where T : MonoBehaviour
+    {
+        protected U Component { get => this.component; }
+        U component = null;
+
+        protected bool Valid { get => this.component != null; }
+
+        protected abstract string Key { get; } 
+       
+        protected override void OnAwake()
+        {
+            base.OnAwake();
+
+            Addressables.LoadAssetAsync<GameObject>(Key).Completed += (asyncHandler) =>
+            {
+                GameObject obj = Instantiate(asyncHandler.Result);
+                obj.transform.SetParent(this.transform);
+
+                this.component = obj.GetComponentInChildren<U>();
+
+                OnComponentLoaded(this.component);
+            };
+        }
+
+        public abstract void OnComponentLoaded(U component);
+    }
+}
---
+++ Scripts/Runtime/Generic Singleton/Singleton.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dubi.Singleton
+{
+    public class Singleton<T> : Singleton where T : MonoBehaviour
+    {
+        private static T instance;
+
+
+        private static readonly object Lock = new object();
+
+        [SerializeField] private bool persistent = true;
+
+        public static T Instance
+        {
+            get
+            {
+                if (Quitting)
+                {
+                    return null;
+                }
+                lock (Lock)
+                {
+                    if (instance != null)
+                    {
+                        return instance;
+                    }
+
+                    T[] instances = FindObjectsOfType<T>();
+                    int count = instances.Length;
+                    if (count > 0)
+                    {
+                        if (count == 1)
+                        {
+                            return instance = instances[0];
+                        }
+
+                        for (int i = count - 1; i > 0; i--)
+                        {
+                            Destroy(instances[i]);
+                        }
+
+                        return instance = instances[0];
+                    }
+
+                    instance = new GameObject("!" + typeof(T).ToString() + " - Singleton").AddComponent<T>(); 
+                    return instance;
+                }
+            }
+        }
+
+        private void Awake()
+        {
+            if (this.persistent)
+            {
+                DontDestroyOnLoad(this.gameObject);
+            }
+
+            OnAwake();
+        }       
+
+        protected virtual void OnAwake() { }
+        public void LoadSingletonObject()
+        {
+            /// Hello reader.
+        }
+    }
+
+    public abstract class Singleton : MonoBehaviour
+    {
+        bool quitting = false;
+
+        public bool LocalQuitting => quitting;
+
+        public static bool Quitting { get; private set; }
+
+        private void OnApplicationQuit()
+        {
+            Quitting = true;
+            quitting = true;
+        }
+    }
+
+}
